d6a87603733cf2756eeae3e590243897da9d07cd
FUELEDBYESPRESSO\leont <xavicano@uw.edu>
2022-09-15 15:25:31 -0700

    Combine artists from following and favorites

--- api/routes.go
+++ api/routes.go
@@ -5,10 +5,12 @@ import (
 	"encoding/json"
 	"github.com/gin-gonic/gin"
 	"github.com/lib/pq"
+	"github.com/mmcloughlin/geohash"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"strings"
 )
 
 // Routes All the routes created by the package nested in
@@ -18,21 +20,14 @@ func Routes(r *gin.RouterGroup, db *database.DB) {
 }
 
 func resourceRoute(r *gin.RouterGroup, db *database.DB) {
+	r.GET("/concertsO", overlappedContent(db))
 	r.GET("/concerts", getConcerts(db))
-	r.GET("/getconcerts", getTicketmasterConcerts())
 }
 
-type resp struct {
-	Href  string   `json:"href"`
-	Items []artist `json:"items"`
-}
-type artist struct {
-	Name string `json:"name"`
-}
-
-func getConcerts(db *database.DB) gin.HandlerFunc {
+func overlappedContent(db *database.DB) gin.HandlerFunc {
 	return func(c *gin.Context) {
 		var artists resp
+
 		session, err := db.SessionStore.Get(c.Request, "session")
 		if err != nil {
 			c.AbortWithStatusJSON(500, "The server was unable to retrieve this session")
@@ -52,20 +47,95 @@ func getConcerts(db *database.DB) gin.HandlerFunc {
 				c.AbortWithStatusJSON(500, "The server was unable to retrieve school info")
 			}
 		}
+		getTopSpotifyArtists(&artists, accessToken)
 
-		req, _ := http.NewRequest("GET", "https://api.spotify.com/v1/me/top/artists", nil)
-		req.Header.Set("Authorization", "Bearer "+accessToken)
-		client := &http.Client{}
-		response, err := client.Do(req)
-		contents, err := ioutil.ReadAll(response.Body)
+		c.JSON(200, "test")
+	}
+}
+
+type following struct {
+	Artists resp `json:"artists"`
+}
+
+type resp struct {
+	Href  string   `json:"href"`
+	Items []artist `json:"items"`
+}
+type artist struct {
+	Name string `json:"name"`
+}
+
+func getTopSpotifyArtists(artists *resp, accessToken string) {
+	req, _ := http.NewRequest("GET", "https://api.spotify.com/v1/me/top/artists?limit=50", nil)
+	req.Header.Set("Authorization", "Bearer "+accessToken)
+	client := &http.Client{}
+	response, err := client.Do(req)
+	contents, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return
+	}
+	err = json.Unmarshal(contents, &artists)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func getFollowedArtists(followingArtists *following, accessToken string) {
+	req, _ := http.NewRequest("GET", "https://api.spotify.com/v1/me/following?type=artist&limit=50", nil)
+	req.Header.Set("Authorization", "Bearer "+accessToken)
+	client := &http.Client{}
+	response, err := client.Do(req)
+	contents, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return
+	}
+	err = json.Unmarshal(contents, &followingArtists)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func getConcerts(db *database.DB) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var artists resp
+		var followingArtist following
+		session, err := db.SessionStore.Get(c.Request, "session")
 		if err != nil {
+			c.AbortWithStatusJSON(500, "The server was unable to retrieve this session")
 			return
 		}
-		err = json.Unmarshal(contents, &artists)
+		spotifyID := session.Values["SpotifyID"]
+
+		accessTokenRow, err := db.Db.Query(`SELECT access_token from account WHERE spotify_id=$1`, spotifyID)
 		if err != nil {
-			log.Println(err)
+			database.CheckDBErr(err.(*pq.Error), c)
+			return
 		}
-		c.JSON(200, artists)
+		var accessToken string
+		for accessTokenRow.Next() {
+			err = accessTokenRow.Scan(&accessToken)
+			if err != nil {
+				c.AbortWithStatusJSON(500, "The server was unable to retrieve school info")
+			}
+		}
+		getTopSpotifyArtists(&artists, accessToken)
+		getFollowedArtists(&followingArtist, accessToken)
+		favArtists := make(map[string]string)
+		for _, item := range artists.Items {
+			favArtists[strings.ToLower(item.Name)] = ""
+		}
+		for _, item := range followingArtist.Artists.Items {
+			favArtists[strings.ToLower(item.Name)] = ""
+		}
+		events := getTicketmasterConcerts()
+		for _, compactEvent := range events {
+			for _, artistPlaying := range compactEvent.Attractions {
+				if _, ok := favArtists[strings.ToLower(artistPlaying)]; ok {
+					favArtists[strings.ToLower(artistPlaying)] = compactEvent.Name
+				}
+			}
+		}
+		c.JSON(200, favArtists)
 	}
 }
 
@@ -110,21 +180,39 @@ func reduceEvents(events Events) []CompactEvent {
 	return compactEvents
 }
 
-func getTicketmasterConcerts() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		var shows Events
-		resp, err := http.Get("https://app.ticketmaster.com/discovery/v2/events.json?size=100&city=seattle&radius=100&units=mi&apikey=" + os.Getenv("TICKETMASTER_KEY"))
-		if err != nil {
-			return
-		}
-		contents, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			return
-		}
-		err = json.Unmarshal(contents, &shows)
-		if err != nil {
-			log.Println(err)
-		}
-		c.JSON(200, reduceEvents(shows))
+func getTicketmasterConcerts() []CompactEvent {
+	var shows Events
+	var shows2 Events
+
+	seattle := geohash.Encode(47.5, -122)
+	seattle = seattle[:5]
+	resp, err := http.Get("https://app.ticketmaster.com/discovery/v2/events.json?size=200&segmentName=Music&geoPoint=" + seattle + "&apikey=" + os.Getenv("TICKETMASTER_KEY"))
+	if err != nil {
+		return []CompactEvent{}
 	}
+	contents, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return []CompactEvent{}
+	}
+	err = json.Unmarshal(contents, &shows)
+	if err != nil {
+		log.Println(err)
+	}
+
+	resp2, err := http.Get("https://app.ticketmaster.com/discovery/v2/events.json?size=200&segmentName=Music&geoPoint=" + seattle + "&apikey=" + os.Getenv("TICKETMASTER_KEY"))
+	if err != nil {
+		return []CompactEvent{}
+	}
+	contents2, err := ioutil.ReadAll(resp2.Body)
+	if err != nil {
+		return []CompactEvent{}
+	}
+	err = json.Unmarshal(contents2, &shows2)
+	if err != nil {
+		log.Println(err)
+	}
+	redEvents := reduceEvents(shows)
+	redEvents2 := append(redEvents, reduceEvents(shows2)...)
+
+	return redEvents2
 }
