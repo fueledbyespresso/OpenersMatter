51342f888a75f647623032ece6addf04c8403d2c
FUELEDBYESPRESSO\leont <xavicano@uw.edu>
2022-11-25 13:17:14 -0800

    Added readme

--- run-build.sh
+++ run-build.sh
@@ -1,2 +1,2 @@
-docker build --build-arg REACT_APP_GOOGLE_MAPS_API_KEY=AIzaSyBvtxpvUnt97HL9AIBi7H4AAHFs3cfeJ1k -t curated-concerts .
+docker build --build-arg REACT_APP_GOOGLE_MAPS_API_KEY={KEY} -t curated-concerts .
 docker run -p 5000:5000 --name curated-concerts curated-concerts
